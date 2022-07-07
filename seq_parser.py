8b2dc74e3fa298a1938d73f1a23ed1e240d91ec0
Reecan J Juarez <43446448+so-dopamine@users.noreply.github.com>
2022-07-07 13:47:09 -0500

    Update seq_parser.py
    Added get_cwd function that moves scaffold produced by scaffold constructor module to user working directory.

--- seq_parser.py
+++ seq_parser.py
@@ -1,6 +1,19 @@
 import sys
-sys.path.insert(0, '/usr/home/LassoHTP/lasso_extension')
+sys.path.insert(0, '/usr/LassoHTP/lasso_extension')
+import subprocess
+import os
 from lasso_peptide_gen import *
+
+def get_cwd(directory):
+    """get directory
+    """
+    
+    if(not directory):
+        cwd = os.getcwd()
+    else:
+        cwd = os.path.abspath(directory) 
+    
+    return cwd
 
 def seq_parse(seq:str, ring_len: int, upper_plug: int):
     
@@ -28,27 +41,23 @@ def seq_flags(seq: str, ring_num: int):
         if idx == ring_num: #acidic residue
             continue
         lis.append('A' + str(idx) + char)
-    #lis = ['A' + str(idx+1) + char for idx, char in enumerate(seq)]
-    #    return 'A' + str(idx+1) + char
     return lis
 
-def construct_scaffold(seq:str, ring_len: int, upper_plug:int):
+def construct_scaffold(seq:str, ring_len: int, upper_plug:int, wk_dir):
     """constructs scaffold from given sequence
     """
-    seq_len=len(seq)
-    ring = ring_len
-    loop = upper_plug
-    isopeptide = ''
-    tail_length = seq_len - ( ring + loop )
-    for idx, char in enumerate(seq):
-        idx +=1
-        if idx == ring:
-           isopeptide = 'e' if char in 'E' else 'd'
+    cwd = get_cwd(wk_dir)
+    
+    seq, ring, loop, tail_length, isopeptide = seq_parse(seq, ring_len, upper_plug)
     outfile = f"{ring}{isopeptide}_{loop}_{tail_length}.pdb"
     lasso_peptide_gen(ring, loop, tail_length, isopeptide, outfile)
+    out_path = "lasso_extension/output_structures/" + outfile
+    outfile_mover(out_path, cwd)   
+ 
+    return out_path
 
-def outfile_pointer():
+def outfile_mover(lasso,wk_dir):
     """moves outfile to working directory.
     """
-    #os
-    pass
+    command = ['cp', lasso, wk_dir]
+    subprocess.run(command)
