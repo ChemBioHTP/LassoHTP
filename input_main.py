db9d6de3315e4be8ddb936305e9b2110d91d6467
Reecan J Juarez <43446448+so-dopamine@users.noreply.github.com>
2022-07-08 12:23:56 -0500

    Update input_main.py

--- input_main.py
+++ input_main.py
@@ -3,14 +3,17 @@ from Class_Conf import *
 from seq_parser import *
 
 def main():
+    #Module 1: Scaffold Constructor
     #specify the annotated sequence and working directory
-        seq, ring_length, upper_plug, wk_dir = 'GGAGHVPEYFVRGDTPISFYG', 8, 11, ''
+        seq, ring_length, upper_plug, wk_dir = '', , , ''
         proto_lasso=construct_scaffold(seq, ring_length, upper_plug, wk_dir)
         print(proto_lasso)
         PDB1=PDB(proto_lasso,wk_dir)
+    #Module 2: Mutation
         sequence=seq_flags(seq, 8) #lasso seq
         print(PDB1.Add_MutaFlag(sequence))
         PDB1.PDB2PDBwLeap()
+    #Module 3: Molecular Dynamics
     #use minimization to relax each mutated lasso
         PDB1.PDB2FF()
         PDB1.PDBMin()
@@ -18,7 +21,7 @@ def main():
         PDB1.rm_wat()
         PDB1.PDB2FF()
         PDB1.conf_prod['nstlim'] = 500 # Edit MD configuration (see default in Class_Conf.py - Config.Amber)
-        PDB1.PDBMD(tag='_mccJ25_RGD') #dir to store MD input files
+        PDB1.PDBMD(tag='_') #dir to store MD input files
 
 if __name__ == "__main__":
     main()
