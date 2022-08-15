7e1191c9ad886a358eb51413a0078e1a22c808c7
Reecan J Juarez <43446448+so-dopamine@users.noreply.github.com>
2022-08-15 16:19:00 -0500

    Update seq_parser.py
    Add locate_tail_ext_module()

--- seq_parser.py
+++ seq_parser.py
@@ -1,34 +1,49 @@
 import sys
-sys.path.insert(0, '/usr/LassoHTP/lasso_extension')
+import shutil
 import subprocess
 import os
-from lasso_peptide_gen import *
 
 def get_cwd(directory):
-    """get directory
+    """gets directory and returns path
     """
-    
     if(not directory):
         cwd = os.getcwd()
     else:
-        cwd = os.path.abspath(directory) 
-    
+        if os.path.isdir(directory):
+            cwd = os.path.abspath(directory)
+            return cwd
+        else:
+            os.mkdir(directory)
+            cwd = os.path.abspath(directory)
+
     return cwd
 
+def locate_tail_ext_module():
+    """gives path of /lasso_extension
+    """
+    lhtp_dir = get_cwd('')
+    tail_ext_dir = lhtp_dir + '/lasso_extension'
+
+    return tail_ext_dir
+
+tail_ext_dir = locate_tail_ext_module()
+sys.path.insert(0,tail_ext_dir)
+from lasso_peptide_gen import *
+
 def seq_parse(seq:str, ring_len: int, upper_plug: int):
-    
+
     sequence = seq
     seq_len = len(sequence)
     ring = ring_len
     loop = upper_plug
     isopeptide = ''
     tail_length = seq_len - ( ring + loop )
-    
+
     for idx , char in enumerate(seq):
         idx += 1
         if idx == ring:
             isopeptide = 'e' if char in 'E' else 'd'
- 
+
     return sequence, ring, loop, tail_length, isopeptide
 
 
@@ -45,19 +60,19 @@ def seq_flags(seq: str, ring_num: int):
 
 def construct_scaffold(seq:str, ring_len: int, upper_plug:int, wk_dir):
     """constructs scaffold from given sequence
-    """
+ """
     cwd = get_cwd(wk_dir)
-    
+
     seq, ring, loop, tail_length, isopeptide = seq_parse(seq, ring_len, upper_plug)
     outfile = f"{ring}{isopeptide}_{loop}_{tail_length}.pdb"
     lasso_peptide_gen(ring, loop, tail_length, isopeptide, outfile)
     out_path = "lasso_extension/output_structures/" + outfile
-    outfile_mover(out_path, cwd)   
- 
+    outfile_mover(out_path, cwd)
+
     return out_path
 
 def outfile_mover(lasso,wk_dir):
     """moves outfile to working directory.
     """
-    command = ['cp', lasso, wk_dir]
-    subprocess.run(command)
+    filePath = shutil.copy(lasso, wk_dir)
+    print(filePath)
